3a9d242445ee11347d5c82f281b334d28909f1b1
AnikHadi <anikhadi9@gmail.com>
2025-04-26 16:07:14 +0600

    Constants & Macro Topic

--- 02/07. Variables & Data Types/Boilerplate Code.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    //  all code here
-    cout << "Enter a number: ";
-    return 0;
-}
---
+++ 03/08. Operators/Constants_Macro.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+#define ll long long // Macro definition
+
+
+int main() {
+    const float PI = 3.14; // Constant declaration
+    const int MAX_SIZE = 100; // Constant declaration
+
+    cout << "Value of PI: " << PI << endl;
+    cout << "Value of MAX_SIZE: " << MAX_SIZE << endl;
+
+
+
+    return 0;
+}
