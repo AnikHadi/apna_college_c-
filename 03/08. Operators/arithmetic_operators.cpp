1edbc80aa60fedef01e6c9a3fce1dee99522d551
AnikHadi <anikhadi9@gmail.com>
2025-04-26 17:33:26 +0600

    Arithmetic Operators

---
+++ 03/08. Operators/arithmetic_operators.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    // Arithmetic Operators 
+        // Binary Operators & Unary Operators
+    // Binary Operators: +, -, *, /, %
+    // Unary Operators: ++, --
+
+    int a = 10, b = 20;
+    cout << "a + b = " << a + b << endl; // Addition
+    cout << "a - b = " << a - b << endl; // Subtraction
+    cout << "a * b = " << a * b << endl; // Multiplication
+    cout << "a / b = " << (double)a / b << endl; // Division
+    cout << "a % b = " << a % b << endl; // Modulus
+    cout << "++a = " << ++a << endl; // Pre-increment
+    cout << "a++ = " << a++ << endl; // Post-increment
+    cout << "a = " << a << endl; // Current value of a
+    cout << "--a = " << --a << endl; // Pre-decrement
+    cout << "a-- = " << a-- << endl; // Post-decrement
+
+
+    return 0;
+}
