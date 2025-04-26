59520054574360de89a7bbe4ec4800bdf823a702
AnikHadi <anikhadi9@gmail.com>
2025-04-26 16:29:21 +0600

    Typecasting

---
+++ 03/08. Operators/typecasting.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   float PI = 3.14; // Constant declaration
+
+   cout << (int)PI << endl; // Typecasting float to int
+
+   cout << ((float)10/3) << endl; // Typecasting int to float
+
+   cout << ((char)('A' + 1)) << endl; // Typecasting char to char
+
+    return 0;
+}
