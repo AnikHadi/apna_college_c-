aca62452f814b913aa594651c0a897eb074517f7
AnikHadi <anikhadi9@gmail.com>
2025-05-08 18:05:41 +0600

    add daimond pattern

--- 06/Exercise/daimond_pattern.cpp
+++ 06/Exercise/daimond_pattern.cpp
@@ -20,34 +20,24 @@ int main() {
     cin >> n;
 
     for (int i = 1; i <= n; i++) {
-        // for (int j = 1; j <= i; j++) {
-        //     if( ){
-        //         cout << "*";
-        //     } else {
-        //         cout << " ";
-        //     }
-        // }
-        // for (int j = 1; j <= i; j++) {
-        //     if( ){
-        //         cout << "*";
-        //     } else {
-        //         cout << " ";
-        //     }
-        // }
-
         for(int j = 1; j <= n; j++) {
             if(n - j >= i) {
-                cout << "  ";
+                cout << " ";
             } else {
                 cout << "* ";
             }
+        }
+        cout << endl;
+    }
 
-            if((i ) == 0 ) {
-                cout << "  ";
-            } else if() {
-                cout << "* ";
-            }
-            
+    
+    for (int i = 1; i <= (n-1); i++) {
+        cout << " ";
+        for(int j = 1; j <= i-1; j++) {
+            cout << " ";
+        }
+        for(int j = 1; j <= (n-i); j++) {
+            cout << "* ";
         }
         cout << endl;
     }
